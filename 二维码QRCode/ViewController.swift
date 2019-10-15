1d9637c92e22fa95e688246642c595e21746ae53
chiyz <3004346919@qq.com>
2019-10-15 14:47:25 +0800

    feat：初始化项目

---
+++ 二维码QRCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  二维码QRCode
+//
+//  Created by chiyz on 2019/10/15.
+//  Copyright © 2019 chiyz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
